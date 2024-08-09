305490cbb11a0e54bb2052923542e0a69051a87b
wagnernazarios <wagnernazario124@gmail.com>
2024-08-08 22:29:39 -0300

    testando Classes

--- java-basico/POO/src/App.java
+++ java-basico/POO/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        Pessoa objetPessoa = new Pessoa();
+
+        System.out.println("Informe seu nome: ");
+        String nome = leitor.nextLine();
+
+        System.out.println("Informe seu peso: ");
+        objetPessoa.setPeso(leitor.nextFloat());
+
+        System.out.println("Informe sua altura: ");
+        objetPessoa.setAltura(leitor.nextFloat());
+
+
+        float imcPessoa = objetPessoa.calcularIMC();
+        System.out.println(nome + ", seu IMC é: " + imcPessoa);
     }
+
 }
---
+++ java-basico/POO/src/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    float peso;
+    float altura;
+
+    public float calcularIMC() {
+        float imc = peso / (altura * altura);
+        return imc;
+    }
+
+}
